89b142cd3d55aa9ef1f2e694076ec810c47d30c1
puneeth-techie <puneethpgowda.techie@gmail.com>
2021-03-15 10:41:50 +0530

    Implemented login logic.

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,40 +1,61 @@
-import User from '../models/userModel.js'
-import generateToken from '../utils/generateToken.js'
-import createError from 'http-errors'
-import { validateUserSchema} from '../utils/validateSchema.js'
+import User from "../models/userModel.js";
+import generateToken from "../utils/generateToken.js";
+import createError from "http-errors";
+import { validateUserSchema } from "../utils/validateSchema.js";
 
 const register = async (req, res, next) => {
-    try{
-        const {error} = await validateUserSchema.validateAsync(req.body)
-        if(error) throw createError.BadRequest(error.details[0].message);
-        const {name, email, password} = req.body
-        let user = await User.findOne({email})
-        if(user) throw createError.Conflict(`${email} already registered. Please login.`)
+  try {
+    const { error } = await validateUserSchema.validateAsync(req.body);
+    if (error) throw createError.BadRequest(error.details[0].message);
+    const { name, email, password } = req.body;
+    let user = await User.findOne({ email });
+    if (user)
+      throw createError.Conflict(`${email} already registered. Please login.`);
 
-        user = await User.create({name, email, password})
-        res.status(201).json({
-            _id: user._id,
-            name: user.name,
-            email: user.email,
-            token: generateToken(user._id)
-        })
-    }catch(error){
-        if(error.isJoi === true) error.status = 422
-        next(error)
+    user = await User.create({ name, email, password });
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id),
+    });
+  } catch (error) {
+    if (error.isJoi === true) error.status = 422;
+    next(error);
+  }
+};
+
+const login = async (req, res, next) => {
+  const { email, password } = req.body;
+  let user = await User.findOne({ email });
+  try {
+    if (!user)
+      throw createError.NotFound(
+        `${email} is not registered. Please register.`
+      );
+    const isMatch = user.isValidPassword(user.password);
+    if (!isMatch) {
+      throw createError.Unauthorized("Invalid email and password.");
+    } else {
+      const token = generateToken(user._id);
+      res.status(201).json({
+        Success: {
+          message: "Successfully logged in.",
+          token: token,
+        },
+      });
     }
-}
-
-const login = (req, res, next) => {
-    res.send('Login route');
-}
-
+  } catch (error) {
+    next(error);
+  }
+};
 
 const logout = (req, res, next) => {
-    res.send('Logout route');
-}
+  res.send("Logout route");
+};
 
 const refreshToken = (req, res, next) => {
-    res.send('RT route');
-}
+  res.send("RT route");
+};
 
-export { register, login, logout, refreshToken}
+export { register, login, logout, refreshToken };
--- models/userModel.js
+++ models/userModel.js
@@ -1,33 +1,38 @@
-import mongoose from 'mongoose'
-import bcrypt from 'bcryptjs'
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
 const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        minlength: 5,
-        maxlength: 255
-    },
-    email: {
-        type: String,
-        required: true,
-        lowercase: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true,
-        minlength: 5,
-        maxlength: 16
-    }
-})
+  name: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 255,
+  },
+  email: {
+    type: String,
+    required: true,
+    lowercase: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 16,
+  },
+});
 
-userSchema.pre('save', async function(next){
-    if(!this.isModified('password')){
-        next();
-    }
-    this.password = await bcrypt.hash(this.password, 10);
-})
-const User = mongoose.model('User', userSchema);
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+userSchema.methods.isValidPassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+const User = mongoose.model("User", userSchema);
 
 export default User;
